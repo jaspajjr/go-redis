4d595954d2d8ce6be676efcf7a2e681de350fcba
John Carney <jaspajjr@gmail.com>
2018-02-04 23:32:01 +0000

    Sets and gets a value in the redis db

--- app.go
+++ app.go
@@ -7,21 +7,43 @@ import (
     "gopkg.in/redis.v3"
 )
 
-func main() {
+func loggerMiddleware(nextHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Request recieved: %v\n", r)
+		nextHandler.ServeHTTP(w, r)
+		fmt.Println("Request handled successfully")
+	})
+}
 
+func indexPageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("indexPageHandler called")
 	client := redis.NewClient(&redis.Options{
 		Addr:     "db:6379",
 		Password: "",
 		DB:       0,
 	})
 
-	pong, err := client.Ping().Result()
+	err := client.Set("key", "foo", 0).Err()
 	if err != nil {
 		panic(err)
 	}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", pong)
-	})
 
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	} else {
+        fmt.Fprintln(w, "value is: %q", val)
+    }
+  }
+
+func main() {
+	fmt.Println("It's running and what not.")
+
+	mux := http.NewServeMux()
+
+
+	mux.Handle("/", loggerMiddleware(http.HandlerFunc(indexPageHandler)))
+	mux.Handle("/about", loggerMiddleware(http.HandlerFunc(indexPageHandler)))
+
+    log.Fatal(http.ListenAndServe(":5000", mux))
 }
